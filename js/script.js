3f51d29e668f6232ed0ca0d586a28067f114ff73
ahmedfarid87 <ahmed123456farid@gmail.com>
2024-06-10 19:43:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,127 @@
+var userNameInput = document.getElementById("userName");
+var userEmailInput = document.getElementById("userEmail");
+var userPassInput = document.getElementById("userPass");
+
+var signEmailInput = document.getElementById("signEmail");
+var signPassInput = document.getElementById("signPass");
+
+
+var users  ;
+if(localStorage.getItem("usersList")==null){
+    var users=[];
+}
+else{
+    users = JSON.parse(localStorage.getItem("usersList"));
+}
+function addUser(){
+    if(!checkIsEmpty()){
+        if(exist()){
+            displayExist();
+        }
+        else{
+        var user = {
+            name:userNameInput.value,
+            email:userEmailInput.value,
+            password:userPassInput.value,
+        }
+        users.push(user);
+        localStorage.setItem("usersList",JSON.stringify(users));
+        displaySucess()
+        
+    }
+        
+    }
+    else{
+        displayRequired();
+    }
+   
+   
+};
+
+addEventListener('load',
+function  welcome(){
+    document.getElementById("welcome").innerHTML = `Welcome ${JSON.parse(localStorage.getItem("homeList"))}`;
+
+})
+function exist(){
+    for(var i=0;i<users.length;i++){
+        if(users[i].email==userEmailInput.value ){
+            return true; 
+        }
+    }
+    return false;
+}
+function existLogin(){
+for(var i=0;i<users.length;i++){
+    if(users[i].email==signEmailInput.value && users[i].password==signPassInput.value){
+        var name=users[i].name;
+        localStorage.setItem("homeList",JSON.stringify(name));
+        location.replace("home.html");
+        return true;
+    }
+}
+};
+function searchUser(){
+    if(checkIsEmptySign()){
+        displayRequiredSign();
+    }
+    else{
+        if( existLogin()){
+
+        }
+        else{
+            displayIncorrect();
+        }
+        
+    }
+
+};
+function clearForm(){
+    userNameInput.value="";
+    userEmailInput.value="";
+    userPassInput.value="";
+};
+
+
+
+// addEventListener('click', function(e){
+//     users.splice(e,1)
+//     localStorage.setItem("homeList",JSON.stringify(name));
+//     displaySucess()
+// })
+
+
+function checkIsEmpty(){
+    if(userNameInput.value!="" && userPassInput.value !="" && userEmailInput.value !=""){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+function checkIsEmptySign(){
+    if(signEmailInput.value =="" || signPassInput.value =="" ){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+function displayRequired(){
+    document.getElementById("required").innerHTML=`<span class=' text-danger'>All inputs is required</span>`;
+};
+function displayExist(){
+    document.getElementById("required").innerHTML=`<span class=' text-danger'>email already exists</span>`;
+
+};
+function displayIncorrect(){
+    document.getElementById("result-sign").innerHTML=`<span class=' text-danger'>incorrect email or password</span>`;
+};
+function displayRequiredSign(){
+    document.getElementById("result-sign").innerHTML=`<span class=' text-danger'>All inputs is required</span>`;
+};
+function displaySucess(){
+    document.getElementById("required").innerHTML=`<span class=' text-success'>Success</span>`;
+};
